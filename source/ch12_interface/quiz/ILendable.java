e7c4a8d970d2b81771bc9a3bf5d581ca4ec289f3
sykim00 <tiffany456@naver.com>
2023-03-08 18:17:13 +0900

    web

---
+++ source/ch12_interface/quiz/ILendable.java
@@ -0,0 +1,9 @@
+package com.lec.quiz;
+
+public interface ILendable {
+	public byte STATE_BORROWED = 1; // "대출중"을 의미
+	public byte STATE_NORMAL = 0; // "대출가능"을 의미
+	public void checkOut(String borrower, String checkOutDate); //대출
+	public void checkIn(); // 반납
+	public void printState(); // "책 번호 책 이름(저자) 대출가능 여부" 출력
+}
