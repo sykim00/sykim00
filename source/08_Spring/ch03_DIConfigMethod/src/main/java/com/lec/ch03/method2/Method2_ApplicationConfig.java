9157ac9503880713c9819f2fe02d8d6a5af3013b
sykim00 <tiffany456@naver.com>
2023-05-09 16:00:51 +0900

    feat

---
+++ source/08_Spring/ch03_DIConfigMethod/src/main/java/com/lec/ch03/method2/Method2_ApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.lec.ch03.method2;
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.lec.ch03.Student;
+import com.lec.ch03.StudentInfo;
+@Configuration
+public class Method2_ApplicationConfig {
+	@Bean
+	public Student student1() {
+		ArrayList<String> hobbies = new ArrayList<String>();
+		hobbies.add("태권도");
+		hobbies.add("마라톤");
+		Student student = new Student("홍길동", 15, hobbies);
+		student.setHeight(170.5);
+		student.setWeight(65);
+		return student;
+	}
+	@Bean
+	public StudentInfo studentInfo() {
+		StudentInfo studentInfo = new StudentInfo();
+		studentInfo.setStudent(student1());
+		return studentInfo;
+	}
+}
