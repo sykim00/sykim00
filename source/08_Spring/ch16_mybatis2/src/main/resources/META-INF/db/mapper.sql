973c1d8cbcf0381e80bbfd11a5fd08865174b8fe
sykim00 <tiffany456@naver.com>
2023-05-11 17:43:29 +0900

    mybatis2_DB

---
+++ source/08_Spring/ch16_mybatis2/src/main/resources/META-INF/db/mapper.sql
@@ -0,0 +1,34 @@
+-- Emp.xml id=empList
+-- 출력순서 : empno순으로
+SELECT * FROM EMP ORDER BY EMPNO;
+SELECT * 
+    FROM (SELECT ROWNUM RN, A.* FROM (SELECT * FROM EMP ORDER BY EMPNO)A)
+    WHERE RN BETWEEN 6 AND 10;
+-- Emp.xml id=empDeptList
+SELECT * FROM 
+    (SELECT ROWNUM RN, A.* 
+        FROM (SELECT E.*, DNAME, LOC FROM EMP E, DEPT D WHERE E.DEPTNO = D.DEPTNO ORDER BY EMPNO)A)
+    WHERE RN BETWEEN 6 AND 10;
+-- Emp.xml id=totCnt
+SELECT COUNT(*) FROM EMP;
+-- Emp.xml id=detail
+SELECT * FROM EMP WHERE EMPNO = 7782;
+-- Emp.xml id=managerList
+SELECT * FROM EMP WHERE EMPNO IN (SELECT MGR FROM EMP);
+-- Emp.xml id=insert
+INSERT INTO EMP VALUES (9000, '홍길동', 'MANAGER', 7902, '23/04/11', 3100, 310, 40);
+-- Emp.xml id=update
+SELECT * FROM EMP;
+UPDATE EMP SET ENAME = '신길동',
+               JOB = 'IT',
+               MGR = 7902,
+               HIREDATE = '23/04/10',
+               SAL = 4000,
+               COMM = 1000,
+               DEPTNO = 40
+            WHERE EMPNO = 9000;
+-- Emp.xml id=delete
+DELETE FROM EMP WHERE EMPNO = 9000;
+-- Dept.xml id=deptList
+SELECT * FROM DEPT;
+DESC EMP;
