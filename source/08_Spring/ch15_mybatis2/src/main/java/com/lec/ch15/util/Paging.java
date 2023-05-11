df3a816a4a5002a3ff8733a17fe9e40258efdbb6
sykim00 <tiffany456@naver.com>
2023-05-11 17:43:01 +0900

    mybatis1_DB

---
+++ source/08_Spring/ch15_mybatis2/src/main/java/com/lec/ch15/util/Paging.java
@@ -0,0 +1,50 @@
+package com.lec.ch15.util;
+
+import lombok.Data;
+
+@Data
+public class Paging {
+	private int currentPage = 1;
+	private int pageSize;
+	private int blockSize;
+	private int startRow;
+	private int endRow;
+	private int totCnt;
+	private int pageCnt;
+	private int startPage;
+	private int endPage;
+	// empList.do 또는 empList.do?pageNum=2
+	// Paging paging = new Paging(14, 2, 10, 10);
+	public Paging(int totCnt, String pageNum, int pageSize, int blockSize) {
+		if(pageNum!=null) {
+			currentPage = Integer.parseInt(pageNum);
+		}
+		this.totCnt = totCnt;
+		this.pageSize = pageSize;
+		this.blockSize = blockSize;
+		startRow = (currentPage-1)*pageSize+1;
+		endRow = startRow+pageSize-1;
+		pageCnt = (int) Math.ceil((double)totCnt/pageSize);
+		startPage = ((currentPage-1)/blockSize)*blockSize+1;
+		endPage = startPage+blockSize-1;
+		if(endPage > pageCnt) {
+			endPage = pageCnt;
+		}
+	}
+	public Paging(int totCnt, String pageNum) {
+		if(pageNum!=null) {
+			currentPage = Integer.parseInt(pageNum);
+		}
+		this.totCnt = totCnt;
+		this.pageSize = 10;
+		this.blockSize = 10;
+		startRow = (currentPage-1)*pageSize+1;
+		endRow = startRow+pageSize-1;
+		pageCnt = (int) Math.ceil((double)totCnt/pageSize);
+		startPage = ((currentPage-1)/blockSize)*blockSize+1;
+		endPage = startPage+blockSize-1;
+		if(endPage > pageCnt) {
+			endPage = pageCnt;
+		}
+	}
+}
